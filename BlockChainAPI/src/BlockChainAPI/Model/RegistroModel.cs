39916897a30ddc02e8a07e8f646cf266dbe6d4ec
hantorr <43685378+hantorr@users.noreply.github.com>
2018-11-19 09:29:55 -0500

    Add files via upload

---
+++ BlockChainAPI/src/BlockChainAPI/Model/RegistroModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChainAPI.Model
+{
+    public class RegistroModel
+    {
+    }
+}
