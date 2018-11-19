b541afc743629eee7aa9b2d2f6477df25d5adac3
hantorr <hantorr@FPB93NZ32>
2018-11-19 17:18:34 -0500

    OK

---
+++ BlockChainAPI/BlockChainFront/Models/RegistroModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockChainFront.Models
+{
+    public class RegistroModel
+    {
+        public string Identificacion { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cuenta { get; set; }
+        public string Direccion { get; set; }
+        public long IdTransaccion { get; set; }
+    }
+}
---
+++ BlockChainAPI/BlockChainFront/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlockChainFront.Startup))]
+namespace BlockChainFront
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
