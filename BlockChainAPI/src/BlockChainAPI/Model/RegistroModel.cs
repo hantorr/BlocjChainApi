cce42d8c2716843a1b066870f92663dced0de35c
hantorr <hantorr@FPB93NZ32>
2018-11-19 17:09:28 -0500

    OK

--- BlockChainAPI/src/BlockChainAPI/Model/RegistroModel.cs
+++ BlockChainAPI/src/BlockChainAPI/Model/RegistroModel.cs
@@ -11,6 +11,9 @@ namespace BlockChainAPI.Model
         public string TipoIdentificacion { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Cuenta { get; set; }
+        public string Direccion { get; set; }
+        public long IdTransaccion { get; set; }
 
     }
 }
