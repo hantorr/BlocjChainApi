018b08ee97b519507dd92928eb8bf1c574534f6d
hantorr <hantorr@FPB93NZ32>
2018-11-26 17:16:22 -0500

    OK

--- BlockChainAPI/BlockChainFront/Controllers/BancoController.cs
+++ BlockChainAPI/BlockChainFront/Controllers/BancoController.cs
@@ -1,67 +1,60 @@
 ﻿using BlockChainFront.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BlockChainFront.Controllers
 {
-    public class RegistroController : Controller
+    public class BancoController : Controller
     {
-
-        private BlockChainModelContainer db = new BlockChainModelContainer();
-
-
-        // GET: Registro
+        // GET: Banco
         public ActionResult Index()
         {
-            return View();
+            SaldoModel saldo = new SaldoModel();
+            saldo.IdBC = "DFGHJK";
+            saldo.Banco = "Finandina";
+            saldo.Saldo = 23456;
+
+            return View(saldo);
         }
 
-        // GET: Registro/Details/5
+        // GET: Banco/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Registro/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        [HttpGet]
-        public ActionResult Create(RegistroModel registro)
+        // GET: Banco/Create
+        public ActionResult Create()
         {
-            try
-            {
-                Cuentas cuentaDB = new Cuentas();
-
-
-                if (ModelState.IsValid)
-                {
-                    db.Cuentas.Add(cuentaDB);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (RetryLimitExceededException /* dex */)
-            {
-                //Log the error (uncomment dex variable name and add a line here to write a log.
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            }
             return View();
         }
 
-        // GET: Registro/Edit/5
+        // POST: Banco/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Banco/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Registro/Edit/5
+        // POST: Banco/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
@@ -77,13 +70,13 @@ namespace BlockChainFront.Controllers
             }
         }
 
-        // GET: Registro/Delete/5
+        // GET: Banco/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Registro/Delete/5
+        // POST: Banco/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
---
+++ BlockChainAPI/BlockChainFront/Models/SaldoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockChainFront.Models
+{
+    public class SaldoModel
+    {
+        public string IdBC { get; set; }
+        public string Banco { get; set; }
+        public float Saldo { get; set; }
+
+    }
+}
