89ff0f5d7696278ed2f46d7e4ca3681de7496e88
hantorr <hantorr@FPB93NZ32>
2018-11-26 19:52:44 -0500

    OK

--- BlockChainAPI/ConexionBC/ConexionBC.cs
+++ BlockChainAPI/ConexionBC/ConexionBC.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.Web3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace ConexionBC
 {
-    public class ConexionBC
+    public class ConexionBC: Web3
     {
+        public ConexionBC(IClient client):base(client)
+        {
+
+        }
+
 
 
 
