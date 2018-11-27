2717aba3d72d0fc692ed6e52160581afa6492544
hans.torres <https://github.com/hantorr/blockchain.git>
2018-11-26 20:20:29 -0500

    OK

---
+++ BlockChainAPI/ConexionBC/QuorumChainService.cs
@@ -0,0 +1,34 @@
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.RPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionBC
+{
+    public class QuorumChainService: RpcClientWrapper
+    {
+        public QuorumChainService(IClient client) : base(client)
+        {
+            CanonicalHash = new QuorumCanonicalHash(client);
+            IsBlockMaker = new QuorumIsBlockMaker(client);
+            IsVoter = new QuorumIsVoter(client);
+            MakeBlock = new QuorumMakeBlock(client);
+            PauseBlockMaker = new QuorumPauseBlockMaker(client);
+            ResumeBlockMaker = new QuorumResumeBlockMaker(client);
+            Vote = new QuorumVote(client);
+            NodeInfo = new QuorumNodeInfo(client);
+        }
+
+        public QuorumCanonicalHash CanonicalHash { get; private set; }
+        public QuorumIsBlockMaker IsBlockMaker { get; private set; }
+        public QuorumIsVoter IsVoter { get; private set; }
+        public QuorumMakeBlock MakeBlock { get; private set; }
+        public QuorumPauseBlockMaker PauseBlockMaker { get; private set; }
+        public QuorumResumeBlockMaker ResumeBlockMaker { get; private set; }
+        public QuorumVote Vote { get; private set; }
+        public QuorumNodeInfo NodeInfo { get; private set; }
+    }
+}
---
+++ BlockChainAPI/ConexionBC/QuorumPauseBlockMaker.cs
@@ -0,0 +1,12 @@
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.RPC.Infrastructure;
+
+namespace ConexionBC
+{
+    public class QuorumPauseBlockMaker : GenericRpcRequestResponseHandlerNoParam<object>
+    {
+        public QuorumPauseBlockMaker(IClient client) : base(client, ApiMethods.quorum_pauseBlockMaker.ToString())
+        {
+        }
+    }
+}
